26267bfe850cda856d38695c497374c9ac760476
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2025-01-05 17:37:48 +0530

    table

---
+++ src/app/dash/page.tsx
@@ -0,0 +1,19 @@
+import { FundTable } from "@/components/dash-fundtable";
+
+export default function Dash() {
+
+    const date = new Date();
+    const month = date.toLocaleString('default', { month: 'long' });
+    const year = date.getFullYear();
+
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
+      <div className="w-full max-w-sm md:max-w-3xl">
+        <h1 className="text-4xl text-center mb-2">
+        {month}
+      </h1>
+        <FundTable />
+      </div>
+    </div>
+  )
+}
